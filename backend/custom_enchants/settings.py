77086abd4c9de1fe4cdedda5fce160861fffae67
BuildTools <coucketimon@gmail.com>
2025-05-07 11:20:07 +0200

    Fix: DEBUG

--- backend/custom_enchants/settings.py
+++ backend/custom_enchants/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-DEBUG = "RENDER" not in os.environ
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a6=r861xvs#$#b5x8v+d=p^+j2cig#w(tl+4r@s*2f7&h=w68o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["localhost", "timonc.be"]
 
